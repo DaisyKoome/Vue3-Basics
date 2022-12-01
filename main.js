3305ee11ff0ff62827c723e1af3359159f99032b
DaisyKoome <dskoome@gmail.com>
2022-12-01 14:16:44 +0300

    Binding html code
    Binding html code

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             message: 'Howdy!',
-            link: 'https://vuejs.org/'
+            link: 'https://vuejs.org/',
+            htmlContent: '<h1>Heey</h1>',
         };
     }
 });
